69d7586c4271d2a60392ae47e47e47714022d55f
Faz <xenorapth@gmail.com>
2020-11-06 12:53:07 +0800

    Add remaining API endpoints

--- app.py
+++ app.py
@@ -45,3 +45,68 @@ def get_payment_methods():
 
     response = requests.post(BASE_URI + PAYMENT_METHODS_EP, headers = HEADERS, json = req)
     return response.json()
+
+@app.route('/api/initiatePayment', method=["POST"])
+def initiate_payment():
+    payment_info = request.get_json()
+    req = {
+        "amount": {
+            "value": 1500,
+            "currency": "SGD"
+        },
+        "channel": "Web",
+        "reference": "test_12",
+        "shopperReference": "Random Shopper",
+        "shopperLocale": "en_US",
+        "countryCode": "SG",
+        "merchantAccount": MERCHANT_ACCOUNT,
+        "additionalData": {
+            "allow3DS2": "true"
+        },
+        "origin": "http://localhost:5000",
+        "returnUrl": "http://localhost:5000/api/handleShopperRedirect"
+    }
+
+    req.update(payment_info)
+    response = requests.post(BASE_URI + PAYMENT_EP, headers = HEADERS, json = req)
+    return response.json()
+
+@app.route('/api/submitAdditionalDetails', methods=["POST"])
+def additional_details():
+    req = request.get_json()
+    response = requests.post(BASE_URI + ADDITIONAL_DETAILS_EP, headers = HEADERS, json = req)
+    return response.json()
+
+@app.route('/api/handleShopperRedirect', methods=["GET", "POST"])
+def handle_redirect():
+    incoming = request.json if request.is_json else request.values.to_dict()
+
+    # Handle Alipay / Wechat
+
+    if incoming['resultCode'] == 'authorised':
+        return redirect(url_for('result', status='success'))
+    elif incoming['resultCode'] == 'refused':
+        return redirect(url_for('result', status='failure'))
+    else:
+        return redirect(url_for('result', status='pending'))
+    
+    # Handle normal flow
+
+    if "paymentData" in incoming:
+        response = requests.post(BASE_URI + ADDITIONAL_DETAILS_EP, headers = HEADERS, json = incoming)
+
+        if response["resultCode"] == "Authorized":
+            return redirect(url_for('result', status='success'))
+        elif response["resultCode"] == "Received" or response["resultCode"] == "Pending":
+            return redirect(url_for('result', status='pending'))
+        else:
+            return redirect(url_for('result', status='failure'))
+    else:
+        return render_template('process.html', incoming = incoming)
+
+@app.route('/result/<status')
+def result(status):
+    return render_template('result.html', status = status)
+
+
+
