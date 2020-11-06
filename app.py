0ae21925df91a6a8594ef4674c23aa3e447b5e73
Faz <xenorapth@gmail.com>
2020-11-06 11:13:23 +0800

    Add basic layout/routing

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+BASE_URI = os.getenv("BASE_URI")
+PAYMENT_METHODS_EP = os.getenv("PAYMENT_METHODS_EP")
+PAYMENT_EP = os.getenv("PAYMENT_EP")
+ADDITIONAL_DETAILS_EP = os.getenv("ADDITIONAL_DETAILS_EP")
+
+API_KEY = os.getenv("API_KEY")
+MERCHANT_ACCOUNT = os.getenv("MERCHANT_ACCOUNT")
+CLIENT_KEY = os.getenv("CLIENT_KEY")
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html', client_key = CLIENT_KEY)
